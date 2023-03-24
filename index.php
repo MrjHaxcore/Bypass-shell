033a87df99460e395eb8cf5c551a4c6ff3f31298
MrjHaxcore <110843759+MrjHaxcore@users.noreply.github.com>
2023-03-24 21:24:07 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,88 @@
+
+<html>
+  <head>
+    <title>.../Hacked by Mrj Haxcore\...</title>
+    <meta name="description" content="sl0w3rs? kurasa tidak">
+		<link href="https://fonts.googleapis.com/css?family=Orbitron&display=swap" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Saira+Stencil+One|Lacquer&display=swap" rel="stylesheet">
+	</head>
+	<style>
+	body{
+	  background-color: black;
+	  color: white;
+	  font-family: Lacquer;
+	}
+	
+	.btn { border: none; width: 200px; height: 30px; outline: none; color: #fff; background: #111; cursor: pointer; position: relative; font-family: Orbitron; z-index: 0; border-radius: 10px; } .btn:before { content: ; background: linear-gradient (45deg, #ff0000, #002bff, #7a00ff, #ff00c8, #ff0000); position: absolute; top: -2px; left: -2px; background-size: 400%; z-index: -1; filter: blur(5px); width: calc(100% + 4px); height: calc(100% + 4px); animation: glowing 20s linear infinite; opacity: 0; transition: opacity .3s ease-in-out; border-radius: 10px; } .btn:active { color: #000; } .btn:active:after { background: transparent; } .btn:hover:before { opacity: 1; } .btn:after { z-index: -1; content: ; position: absolute; width: 100%; height: 100%; background: #111; left: 0; top: 0; border-radius: 10px; }
+	blink {
+	-webkit-animation: 2s linear infinite kedip; /* for Safari 4.0 - 8.0 */
+	animation: 2s linear infinite kedip;
+	}
+	
+	/* for Safari 4.0 - 8.0 */
+	@-webkit-keyframes kedip { 
+	0% {
+	visibility: hidden;
+	}
+	50% {
+	visibility: hidden;
+	}
+	100% {
+	visibility: visible;
+	}
+	}
+	
+	@keyframes kedip {
+	0% {
+	visibility: hidden;
+	}
+	50% {
+	visibility: hidden;
+	}
+	100% {
+	visibility: visible;
+	}
+	blink {
+	-webkit-animation: 2s linear infinite kedip; /* for safari 4.0 - 8.0 */
+	animation: 2s linear infinite kedip;
+	}
+	
+	/* for safari 4.0 - 8.0 */
+	@-webkit-keyframes kedip { 
+	0% {
+	visibility: hidden;
+	}
+	50% {
+	visibility: hidden;
+	}
+	100% {
+	visibility: visible;
+	}
+	}
+	
+	@keyframes kedip {
+	0% {
+	visibility: hidden;
+	}
+	50% {
+	visibility: hidden;
+	}
+	100% {
+	visibility: visible;
+	}
+	}
+	</style>
+	 <script> function play() { var audio = document.getElementById("lagu"); audio.play(); } function pause() { var audio = document.getElementById("lagu"); audio.pause(); } </script> 
+	 	<script src="https://cdn.rawgit.com/bungfrangki/efeksalju/2a7805c7/efek-salju.js" type="text/javascript"></script>
+	<body><table height="100%" width="100%">
+	<td align="center"><center>
+	  <img src="https://ajcoderblog.files.wordpress.com/2016/07/gffn08tb4ccfzhzgw6wvshwwqo6lmluq.gif?w=244" width="550px"><br>
+	  <font size="7" color="blue">[</font></font><blink><font size="7">Hacked by Mrj Haxcore</font></blink><font size="7" color="blue">]</font></font><br><br>
+	  <font size="5" style="font-family: Courier"><b>This is For You Web Administrator  </br> <br>I just want to inform you that I <br> found vulnerable that results to your web security failure   <br>Delete this or else your web page will be compromise  </b><br><br></font>
+  <font size="5" style="font-family: Courier">
+<br>Thanks To : <font size="5" style="font-family: Courier"; color="blue"><b>[</b></font><i><marquee width="60%" scrollamount="10"> SkidSecPhilippines - Anonymous Philippine </marquee></i><font size="5" style="font-family: Courier"; color="blue"><b>]</b></font></font>
+	  <br><br>
+	  <button class="btn" onclick="play()"><font size="3"><font family="Orbitron">Play Music</font></button> &nbsp;&nbsp; <button class="btn" onclick="pause()"><font size="3">Pause Music</font></font></button> &nbsp;&nbsp; 
+      <audio id="lagu" src="https://j.top4top.io/m_2632yb5mf0.mp3"></audio>
+      </body></html>
+	  
